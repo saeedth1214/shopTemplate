a00186a02fea4716af303ef9abb06efea61bcede
saeedsoltani <m.h.soltani1214@gmail.com>
2021-08-12 15:23:15 +0430

    layout  was update

--- resources/views/Layout/layout.blade.php
+++ resources/views/Layout/layout.blade.php
@@ -5,11 +5,11 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <link rel="stylesheet" href="/css/app.css">
+  <link rel="stylesheet" href="/public/css/app.css">
     <title>Document</title>
 </head>
 <body class="sidebar-fixed sidebar-dark header-light header-fixed rtl" id="body" >
     @yield('content')
-       <script src="/js/app.js"></script>
+       <script src="/public/js/app.js"></script>
 </body>
 </html>
